89d0057976b703531b4f076a77df248d5c722492
xuchichi <1516230950@qq.com>
2018-01-02 23:07:08 +0800

    h

--- app/src/main/java/com/custom/cniaoshopingmall/widget/ClearEditText.java
+++ app/src/main/java/com/custom/cniaoshopingmall/widget/ClearEditText.java
@@ -1,15 +1,21 @@
 package com.custom.cniaoshopingmall.widget;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 import android.widget.EditText;
+
+import com.custom.cniaoshopingmall.R;
 
 /**
  * Created by xuchichi on 2018/1/1.
  */
 
 public class ClearEditText extends AppCompatEditText {
+    private Drawable mClearTextIcon;
     public ClearEditText(Context context){
         this(context,null);
     }
@@ -20,5 +26,23 @@ public class ClearEditText extends AppCompatEditText {
 
     public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    private void init(final Context context) {
+
+        final Drawable drawable = ContextCompat.getDrawable(context, R.drawable.icon_delete_32);
+        final Drawable wrappedDrawable = DrawableCompat.wrap(drawable); //Wrap the drawable so that it can be tinted pre Lollipop
+        DrawableCompat.setTint(wrappedDrawable, getCurrentHintTextColor());
+        mClearTextIcon = wrappedDrawable;
+        mClearTextIcon.setBounds(0, 0, mClearTextIcon.getIntrinsicHeight(), mClearTextIcon.getIntrinsicHeight());
+        setClearIconVisible(false);
+//        super.setOnTouchListener(this);
+//        super.setOnFocusChangeListener(this);
+//        addTextChangedListener(this);
+    }
+    private void setClearIconVisible(final boolean visible) {
+        mClearTextIcon.setVisible(visible, false);
+        final Drawable[] compoundDrawables = getCompoundDrawables();
+        setCompoundDrawables(compoundDrawables[0], compoundDrawables[1], visible ? mClearTextIcon : null, compoundDrawables[3]);
     }
 }
